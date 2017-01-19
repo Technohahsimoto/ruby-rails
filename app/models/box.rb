2bfc802382b339d96ade8928b52eeb2b8ee1b770
ryohei <halohs20192@gmail.com>
2017-01-19 07:03:26 +0000

    first commit

---
+++ app/models/box.rb
@@ -0,0 +1,6 @@
+class Box < ApplicationRecord
+    
+    has_many :items
+    
+    validates :name, presence: true
+end
---
+++ app/models/item.rb
@@ -0,0 +1,7 @@
+class Item < ApplicationRecord
+   belongs_to :box, optional: true
+   
+   
+   validates :name, presence: true
+
+end
